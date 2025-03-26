d95784b8b03b2fcccf2c1b10ce0ba9457fd59041
Maria Michailidi <mmichail@fu-c2a02-37-01.cms>
2025-03-26 16:03:25 +0100

    run.sh bash script

---
+++ run.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+rm -rf build
+
+cd /data/cmssw/el8_amd64_gcc12/cms/cmssw/CMSSW_15_0_0_pre2
+eval `scram runtime -sh`
+CUDA_BASE=$(scram tool tag cuda CUDA_BASE)
+TBB_BASE=$(scram tool tag tbb TBB_BASE)
+BOOST_BASE=$(scram tool tag boost BOOST_BASE)
+ALPAKA_BASE=$(scram tool tag alpaka ALPAKA_BASE)
+cd -
+
+
+cmake \
+  -DCMAKE_PREFIX_PATH="${BOOST_BASE};${TBB_BASE};${CUDA_BASE}" \
+  -DCHECK_CUDA_VERSION=OFF \
+  -DALPAKA_DIR="${ALPAKA_BASE}" \
+  -B build \
+  -S . \
+  -L
+
+cmake --build build
