2ba58e4459f9ce3e0b94ad7322ba09ade14ad969
mariamiiiiii <maria-michailidi@outlook.com.gr>
2025-08-19 17:38:30 +0200

    approval

--- src/main.cc
+++ src/main.cc
@@ -453,7 +453,7 @@ int main(int argc, char *argv[]) {
   bool use_accelerator = false;
   bool verbose = false;
   float dc = 20.f, rhoc = 80.f, outlierDeltaFactor = 2.f;
-  int repeats = 10;
+  int repeats = 100;
   int TBBNumberOfThread = 1;
   int opt;
   std::string inputFileName;
