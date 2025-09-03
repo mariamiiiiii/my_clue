77c183e0676b64dcfdf6385d515f18fd2550f12d
Andrea Bocci <andrea.bocci@cern.ch>
2025-09-03 19:22:13 +0200

    Run with 1000 "events"

--- run.sh
+++ run.sh
@@ -5,11 +5,8 @@ rm -rf build
 CUDA_BASE=/usr/local/cuda-12.8
 ROCM_BASE=/opt/rocm-6.4.1
 
-
-
 cmake \
-  -DCMAKE_PREFIX_PATH="${CUDA_BASE}" \
-  -DCHECK_CUDA_VERSION=OFF \
+  -DCMAKE_CUDA_COMPILER="${CUDA_BASE}"/bin/nvcc \
   -DCMAKE_CUDA_ARCHITECTURES=89 \
   -B build \
   -S . \
@@ -17,9 +14,8 @@ cmake \
 
 cmake --build build
 
-rm -f ../Results/results_unified*.csv
-
+rm -f Results/*.csv
 for i in {0..10}
 do
-  ./build/src/clue/main -i data/input/aniso_1000.csv -d 7.0 -r 10.0 -o 2 -e 10 -v -u $i
+  ./build/src/clue/main -i data/input/aniso_1000.csv -d 7.0 -r 10.0 -o 2 -e 1000 -v -u $i
 done
